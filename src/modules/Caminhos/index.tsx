14a44154d44474e4a703ec076cad09127b809d82
Diego Nascimento <diegostealth3@gmail.com>
2024-10-19 10:00:14 -0300

    feat: more accordions

--- src/modules/Caminhos/index.tsx
+++ src/modules/Caminhos/index.tsx
@@ -12,9 +12,18 @@ import {
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { MaxWidth } from '@/shared/components/MaxWidth';
+import { useState } from 'react';
 
 export const Caminhos = () => {
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
   const theme = useTheme();
+
   return (
     <Stack
       pr={2}
@@ -37,10 +46,10 @@ export const Caminhos = () => {
             }}
             sx={{
               minHeight: 500,
-              backgroundImage:
-                'url(https://img.freepik.com/fotos-gratis/arte-digital-cao-bonito_23-2151150544.jpg?w=996&t=st=1728914404~exp=1728915004~hmac=a01769b4c5b78c0a6a0b7b2210bd3c7f6aad18d0f3761fe8e77713012b68d9c6)',
-              backgroundPosition: 'center',
+              backgroundImage: 'url(/oria.png)',
+              backgroundPosition: 'top',
               backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
 
               [theme.breakpoints.up('md')]: {
                 minHeight: 700,
@@ -53,6 +62,7 @@ export const Caminhos = () => {
               lg: 6,
             }}
             paddingBottom={4}
+            marginTop={4}
           >
             <Typography
               textTransform={'uppercase'}
@@ -74,26 +84,35 @@ export const Caminhos = () => {
                 },
                 marginTop: 4,
               }}
+              expanded={expanded === 'panel1'}
+              onChange={handleChange('panel1')}
             >
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                <Stack paddingY={2}> Opcao 1</Stack>
+                <Stack paddingY={2}>Pré-Natal psicologico</Stack>
               </AccordionSummary>
               <AccordionDetails>
                 <AccordionDetails>
-                  <iframe
-                    width="560"
-                    height="315"
+                  <Stack
+                    component={'iframe'}
+                    sx={{
+                      width: '100%',
+                      height: 350,
+
+                      [theme.breakpoints.down('lg')]: {
+                        height: 320,
+                      },
+                    }}
                     src="https://www.youtube.com/embed/FHHoA_qUl8U?si=ZnaLshsS5clQtBNb"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerPolicy="strict-origin-when-cross-origin"
                     allowFullScreen
-                  ></iframe>
+                  />
                 </AccordionDetails>
               </AccordionDetails>
             </Accordion>
@@ -103,25 +122,106 @@ export const Caminhos = () => {
                   transition: 'background .5s',
                 },
               }}
+              expanded={expanded === 'panel2'}
+              onChange={handleChange('panel2')}
             >
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                <Stack paddingY={2}> Opcao 1</Stack>
+                <Stack paddingY={2}>Luto</Stack>
               </AccordionSummary>
               <AccordionDetails>
-                <iframe
-                  width="560"
-                  height="315"
+                <Stack
+                  component={'iframe'}
+                  sx={{
+                    width: '100%',
+                    height: 350,
+
+                    [theme.breakpoints.down('lg')]: {
+                      height: 320,
+                    },
+                  }}
                   src="https://www.youtube.com/embed/FHHoA_qUl8U?si=ZnaLshsS5clQtBNb"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
-                ></iframe>
+                />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              sx={{
+                '&:hover': {
+                  transition: 'background .5s',
+                },
+              }}
+              expanded={expanded === 'panel3'}
+              onChange={handleChange('panel3')}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                <Stack paddingY={2}>Perinatal</Stack>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Stack
+                  component={'iframe'}
+                  sx={{
+                    width: '100%',
+                    height: 350,
+
+                    [theme.breakpoints.down('lg')]: {
+                      height: 320,
+                    },
+                  }}
+                  src="https://www.youtube.com/embed/FHHoA_qUl8U?si=ZnaLshsS5clQtBNb"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              sx={{
+                '&:hover': {
+                  transition: 'background .5s',
+                },
+              }}
+              expanded={expanded === 'panel4'}
+              onChange={handleChange('panel4')}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                <Stack paddingY={2}>Psicoterapia</Stack>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Stack
+                  component={'iframe'}
+                  sx={{
+                    width: '100%',
+                    height: 350,
+
+                    [theme.breakpoints.down('lg')]: {
+                      height: 320,
+                    },
+                  }}
+                  src="https://www.youtube.com/embed/4b2_X3_beuo?si=n8h-BjdFtXdIkbTg"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                />
               </AccordionDetails>
             </Accordion>
           </Grid2>
