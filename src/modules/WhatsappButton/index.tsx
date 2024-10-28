8cfec6f22803485256bae0055725d512e6e24164
Diego Nascimento <diegostealth3@gmail.com>
2024-10-28 11:39:56 -0300

    fix: whatsapp button

--- src/modules/WhatsappButton/index.tsx
+++ src/modules/WhatsappButton/index.tsx
@@ -1,20 +1,33 @@
 'use client';
-import { FloatingWhatsApp } from '@carlos8a/react-whatsapp-floating-button';
-
+import { Avatar, Box, IconButton, useTheme } from '@mui/material';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import Link from 'next/link';
 export const WhatsappButton = () => {
+  const theme = useTheme();
   return (
-    <div>
-      <FloatingWhatsApp
-        phoneNumber="5532999658169" // Required
-        accountName="Ariane Miranda" // Optional
-        avatar="/oria-whatsapp.png" // Optional
-        initialMessageByServer="Olá! Como posso te ajudar??" // Optional
-        initialMessageByClient="Alo gata, preciso dessa mensagem inicial pro whatsapp pra por aqui" // Optional
-        statusMessage="Disponível" // Optional
-        startChatText="Comece a falar comigo" // Optional
-        tooltipText="Precisa de ajuda! Fale comigo" // Optional
-        allowEsc={true}
-      />
-    </div>
+    <Box
+      sx={{
+        position: 'fixed',
+        bottom: 4,
+        right: 8,
+        zIndex: theme.zIndex.drawer - 1,
+      }}
+    >
+      <IconButton
+        component={Link}
+        href="https://wa.me/message/5MAMLEFBR547D1"
+        target="_blank"
+      >
+        <Avatar
+          sx={{
+            backgroundColor: '#25d366',
+            width: 60,
+            height: 60,
+          }}
+        >
+          <WhatsAppIcon fontSize="large" />
+        </Avatar>
+      </IconButton>
+    </Box>
   );
 };
