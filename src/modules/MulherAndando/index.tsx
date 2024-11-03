fc602201cc919dc4f83a854b9e1f8f70efa4c6c5
Diego Nascimento <diegostealth3@gmail.com>
2024-11-03 10:19:54 -0300

    a muie ta melhor agora

--- src/modules/MulherAndando/index.tsx
+++ src/modules/MulherAndando/index.tsx
@@ -2,88 +2,86 @@
 
 import { MaxWidth } from '@/shared/components/MaxWidth';
 import { Box, Stack, Typography, useTheme } from '@mui/material';
-import { motion, useAnimation, useInView } from 'framer-motion';
-import { useEffect, useRef } from 'react';
 
 export const MulherAndando = () => {
   const theme = useTheme();
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount: 0.5 });
-  const mainControls = useAnimation();
 
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start('visible');
-    }
-  }, [isInView, mainControls]);
   return (
-    <Stack
-      ref={ref}
-      position={'relative'}
-      paddingY={{
-        xs: 16,
-        lg: 20,
-      }}
-      paddingX={2}
-      bgcolor={'#aaa'}
-      overflow={'clip'}
-      height={{ xs: 600, md: 800 }}
-    >
-      <MaxWidth>
-        <Stack alignItems={'flex-end'} zIndex={20} justifyContent={'center'}>
+    <Stack>
+      <Stack
+        position={'relative'}
+        sx={{
+          backgroundImage: 'url(videomuie.mp4)',
+          overflow: 'hidden',
+        }}
+      >
+        <MaxWidth>
           <Stack
-            component={motion.div}
-            variants={{
-              hidden: { opacity: 0, x: 40, filter: 'blur(1rem)' },
-              visible: { opacity: 1, x: 0, filter: 'blur(0rem)' },
+            paddingX={2}
+            paddingY={{
+              xs: 8,
+              md: 16,
             }}
-            initial={'hidden'}
-            animate={mainControls}
-            transition={{
-              duration: 1,
-              delay: 0.3,
-              ease: 'easeOut',
-            }}
-            bgcolor={theme.palette.background.paper}
-            borderRadius={4}
-            padding={4}
-            maxWidth={{
-              xs: '400px',
-              lg: '35vw',
-            }}
+            alignItems={'flex-end'}
+            justifyContent={'center'}
           >
-            <Typography
-              fontSize={{
-                xs: 20,
-                lg: 32,
-              }}
-              textAlign={'right'}
-            >
-              Mesmo que você{' '}
-              <Box component={'strong'} color={'#001f3f'}>
-                acredite
-              </Box>{' '}
-              que não ​existe{' '}
-              <Box component={'strong'} color={'#001f3f'}>
-                saída
-              </Box>
-              , saiba que é{' '}
-              <Box component={'strong'} color={'#001f3f'}>
-                possível​ construir
-              </Box>{' '}
-              uma realidade mais​{' '}
-              <Box component={'strong'} color={'#001f3f'}>
-                funcional
-              </Box>{' '}
-              e com mais <strong>qualidade ​de vida</strong>.
-            </Typography>
+            <Stack justifyContent={'center'}>
+              <Stack alignItems={'flex-end'}>
+                <Stack
+                  bgcolor={theme.palette.background.paper}
+                  borderRadius={4}
+                  padding={4}
+                  maxWidth={{
+                    xs: '400px',
+                    lg: '35vw',
+                  }}
+                >
+                  <Typography
+                    fontSize={{
+                      xs: 20,
+                      lg: 32,
+                    }}
+                    textAlign={'right'}
+                  >
+                    Mesmo que você{' '}
+                    <Box component={'strong'} color={'#001f3f'}>
+                      acredite
+                    </Box>{' '}
+                    que não ​existe{' '}
+                    <Box component={'strong'} color={'#001f3f'}>
+                      saída
+                    </Box>
+                    , saiba que é{' '}
+                    <Box component={'strong'} color={'#001f3f'}>
+                      possível​ construir
+                    </Box>{' '}
+                    uma realidade mais​{' '}
+                    <Box component={'strong'} color={'#001f3f'}>
+                      funcional
+                    </Box>{' '}
+                    e com mais <strong>qualidade ​de vida</strong>.
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Stack>
           </Stack>
+        </MaxWidth>
+        <Stack
+          sx={{
+            width: '130vw',
+            objectFit: 'cover',
+            position: 'absolute',
+            left: '0',
+            right: '0',
+            top: '40',
+            bottom: '0',
+            zIndex: '-1',
+          }}
+        >
+          <video autoPlay loop muted>
+            <source src={'videomuie.mp4'} type="video/mp4" />
+          </video>
         </Stack>
-      </MaxWidth>
-      <Stack position={'absolute'} top={-60} left={0}>
-        <video autoPlay loop muted style={{ minWidth: '100vw' }}>
-          <source src={'videomuie.mp4'} type="video/mp4" />
-        </video>
       </Stack>
     </Stack>
   );
--- src/shared/components/MaxWidth/index.tsx
+++ src/shared/components/MaxWidth/index.tsx
@@ -3,14 +3,15 @@ import { PropsWithChildren } from 'react';
 
 export const MaxWidth = ({ children }: PropsWithChildren) => {
   return (
-    <Stack width={'100%'} alignItems={'center'}>
+    <Stack width={'100%'} alignItems={'center'} height={'100%'}>
       <Stack
         width={'100%'}
         maxWidth={{
-          xs: '100',
+          xs: '100%',
           md: '1440px',
           xl: '70%',
         }}
+        height={'100%'}
       >
         {children}
       </Stack>
